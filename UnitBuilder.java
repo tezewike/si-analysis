3a452b2de5ece4242f05a68ea302a2147d3f072e
Tobechukwu Ezewike <tezewike.dev@gmail.com>
2017-07-13 12:18:25 -0400

    Create UnitBuilder.java

---
+++ UnitBuilder.java
@@ -0,0 +1,30 @@
+public class UnitBuilder {
+    private String[] names = null;
+    private String[] symbols = null;
+    private double magnitude = 1.0;
+    private baseUnitArray = null;
+    
+    public UnitBuilder withNames(String[] names) {
+        this.names = names;
+        return this;
+    }
+    
+    public UnitBuilder withSymbols(String[] symbols) {
+        this.symbols = symbols;
+        return this;
+    }
+    
+    public UnitBuilder withMagnitude(String[] magnitude) {
+        this.magnitude = magnitude;
+        return this;
+    }
+    
+    public UnitBuilder withUnitArray(String[] unitArray) {
+        this.names = unitArray;
+        return this;
+    }
+
+    // TODO
+    public Unit build() {
+        return null;
+    }
