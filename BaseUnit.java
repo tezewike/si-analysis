10645553374d0859dc7864ecdff767c5448c25de
Tobechukwu Ezewike <tezewike.dev@gmail.com>
2017-07-13 14:30:38 -0400

    Update BaseUnit.java

--- BaseUnit.java
+++ BaseUnit.java
@@ -21,21 +21,33 @@ public class BaseUnit {
             return this.ordinal();
         }
         
+        public int[] getBaseArray() {
+            int[] array = new int[ENUM_SIZE];
+            array[this.getIndex()] = 1;
+            return array;
+        }
+        
+        public int[] getBaseArray(int exp) {
+            int[] array = new int[ENUM_SIZE];
+            array[this.getIndex()] = exp;
+            return array;
+        }
+        
         @Override
         public String toString() {
             return this.name;
         }
         
+    }
+        
     public enum Dimensions {
-        LENGTH("Length"),
+        LENGTH("Length", ),
         MASS("Mass"),
         TIME("Time"),
         CURRENT("Electric Current"),
         TEMPURATURE("Thermodynamic Temperature"),
         AMOUNT("Amount of Substance"),
         LUMINOSITY("Luminous Intensity");
-        
-        
         
         private int[] assignArray(int[]... array) {
             final int ENUM_INDEX = 0;
@@ -51,5 +63,6 @@ public class BaseUnit {
         }
         
     }
-        
+    
+    
 }
