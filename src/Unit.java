c38ce34346284772e73c1bad1718f13131848883
Tobechukwu Ezewike <tezewike.dev@gmail.com>
2017-07-17 11:36:02 -0400

    Rolled Unit.java and UnitSystem.java into one

--- src/Unit.java
+++ src/Unit.java
@@ -62,7 +62,41 @@ public class Unit {
     public static List<Unit> getAll() {
         return new ArrayList<Unit>(unitTable.values());
     }
-
+    
+    public enum System {
+        INTERNATIONAL_SYSTEM("International System of Units", "m", "kg", "s", "A", "K", "mol", "cd");
+        
+        private final String name;
+        private final Unit[] units;
+        
+        private System(String name; String... unitKeys) {
+            if (unitKeys.length != DimensionArray.ARRAY_SIZE)
+                throw new Exception("invalid arguments for unit system");
+            
+            Unit unit = null;
+            for (int i = 0; i < unitKeys.length) {
+                unit = Unit.get(unitKeys[i]);
+                if (unit == null)
+                    throw new Exception("invalid key, \"" + unitKeys[i] + "\", for unit system");
+                else 
+                    this.units[i] = unit;
+            }
+            
+            this.name = name;
+        }
+        
+        public static converTo(System system, douvble magnitude) {
+            for (Unit unit : units)
+                magnitude *= unit.getMagnitude();
+            return magnitude;
+        }
+        
+        public static Unit[] getUnits(System system) {
+            return units;
+        }
+        
+    }
+    
     static final class UnitComparator implements Comparator<Unit> {
 
         @Override
